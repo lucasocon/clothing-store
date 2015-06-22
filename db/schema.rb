c8e1bd2d2eb77cf65d8ab3110bb8bc8a48d9ae7f
Lucas Ocon <lucas_masterclas@hotmail.com>
2015-06-22 10:34:57 -0300

    Setup pg on project

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150622131913) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "brands", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
