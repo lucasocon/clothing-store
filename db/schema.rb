a7cf33b77598faf1b4404f20dbd12a4a98b8e37a
Lucas Ocon <lucas_masterclas@hotmail.com>
2015-06-22 10:20:46 -0300

    Add Clothing Tyoe model

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150622131729) do
+ActiveRecord::Schema.define(version: 20150622131913) do
 
   create_table "brands", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "clothing_types", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
